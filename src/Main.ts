2ae6415d94f7d186413c5395252ef677f32a5cdf
GuillermoSosa <guillermososaalmeida@gmail.com>
2024-06-06 22:50:21 +0200

    feat: added logged menu functionality for displaying different ordered lists by stats closes #7

--- src/Main.ts
+++ src/Main.ts
@@ -1,13 +1,16 @@
 import * as readline from "readline";
 import { Player } from "./Player";
 import { PlayerLoader } from "./PlayerLoader";
+import { ListBuilder } from "./ListBuilder";
 
 export class Main {
   private players: Player[] = [];
   private playerLoader: PlayerLoader;
+  private listBuilder: ListBuilder;
 
   constructor() {
     this.playerLoader = new PlayerLoader();
+    this.listBuilder = new ListBuilder();
   }
 
   start() {
@@ -16,6 +19,8 @@ export class Main {
   }
 
   private logMenu() {
+    let list: { [key: string]: number | string }[];
+
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
@@ -40,41 +45,49 @@ export class Main {
       (answer) => {
         switch (answer) {
           case "1":
+            list = this.listBuilder.listByGoals(this.players);
             console.log(`
 LISTADO DE GOLES
               `);
             break;
           case "2":
+            list = this.listBuilder.listByShots(this.players);
             console.log(`
 LISTADO DE DISPAROS
               `);
             break;
           case "3":
+            list = this.listBuilder.listByPasses(this.players);
             console.log(`
 LISTADO DE PASES
               `);
             break;
           case "4":
+            list = this.listBuilder.listByAssists(this.players);
             console.log(`
 LISTADO DE ASISTENCIAS
               `);
             break;
           case "5":
+            list = this.listBuilder.listByInterceptions(this.players);
             console.log(`
 LISTADO DE RECUPERACIONES
               `);
             break;
           case "6":
+            list = this.listBuilder.listByYellowCards(this.players);
             console.log(`
 LISTADO DE TARJETAS AMARILLAS
               `);
             break;
           case "7":
+            list = this.listBuilder.listByRedCards(this.players);
             console.log(`
 LISTADO DE TARJETAS ROJAS
               `);
             break;
           case "8":
+            list = this.listBuilder.listByFouls(this.players);
             console.log(`
 LISTADO DE FALTAS
               `);
@@ -93,6 +106,10 @@ LISTADO DE FALTAS
             return;
         }
 
+        for (const [index, player] of list.entries()) {
+          const keys = Object.keys(player);
+          console.log(`${index + 1}. ${player.name}  -->  ${player[keys[1]]}`);
+        }
         rl.question(
           `
 Presiona Enter para volver al menú...`,
